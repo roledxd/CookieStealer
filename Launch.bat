1d318149ac0d62b5ef29d8993e872b04e412361f
RoLedXD <44301044+roledxd@users.noreply.github.com>
2020-01-08 23:19:22 +0200

    Launch.bat fix
    Fixed creating directory for logs

--- Launch.bat
+++ Launch.bat
@@ -1,4 +1,5 @@
 wscript.exe \addon.vbs file.bat
+md %~d0\adv\logs
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
 set mytime=%mytime: =% 
